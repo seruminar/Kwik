ca82a60f4d826eb00d7aebbc2e1982d2f85c4b42
Yuriy Sountsov <yuriys@kentico.com>
2021-02-25 13:28:12 -0500

    Remove waiter, add visibility toggles and improve appearance

--- Client/src/routes/index.svelte
+++ Client/src/routes/index.svelte
@@ -1,82 +1,103 @@
-<script lang="ts">
-  import { stores } from "@sapper/app";
-  import { translate } from "../utilities/translateStore";
-  import { debounce, throttle } from "lodash";
-  import wretch from "wretch";
-  import { fly } from "svelte/transition";
-  import Loading from "../shared/loading.svelte";
-  import type { ISession } from "../shared/session";
-  import type { IQnASearchResultList } from "../shared/qnaMaker";
-  import { onMount } from "svelte";
-
-  interface IAnswerGroup {
-    question: string;
-    answers: IAnswer[];
-    id: number;
-  }
-
-  interface IAnswer {
+<script context="module" lang="ts">
+  export interface IAnswer {
     answer: string;
     questions: string[];
     score: number;
     id: number;
   }
 
-  const { session } = stores<ISession>();
+  export interface IAnswerGroup {
+    question: string;
+    answers: IAnswer[];
+    id: number;
+  }
+</script>
 
-  let ready = false;
-  onMount(() => (ready = true));
+<script lang="ts">
+  import { translate } from "../shared/stores/translate";
+  import { property } from "../shared/actions/property";
+  import { debounce, throttle } from "lodash";
+  import wretch from "wretch";
+  import { fly } from "svelte/transition";
+  import { flip } from "svelte/animate";
+  import Loading from "../shared/components/loading.svelte";
+  import type { IQnASearchResultList } from "../shared/qnaMaker";
+  import { onMount } from "svelte";
+  import { localStorage } from "../shared/stores/localStorage";
+  import Answer from "../shared/components/answer.svelte";
+
+  enum LocalStorageKeys {
+    SavedExport = "Kwik:ShowQuestions",
+    AnswerCount = "Kwik:AnswerCount",
+    CollapsePrevious = "Kwik:CollapsePrevious",
+  }
+
+  interface IQuestionAnswer {
+    question: string;
+    answer: IAnswer;
+  }
 
   let answerGroups: IAnswerGroup[] = [];
-  let trainingPairs: Map<
+  let collapsedAnswerGroupIds: number[] = [];
+  let questionAnswers: Map<number, IQuestionAnswer> = new Map<
     number,
-    { question: string; answer: IAnswer }
-  > = new Map<number, { question: string; answer: IAnswer }>();
+    IQuestionAnswer
+  >();
 
   let questions: string = "";
+  let copied: { top: string; left: string };
   let loading: boolean;
+  let ready = false;
 
-  const sendingInStart = 5000;
+  $: answerCountStore = localStorage<number>(LocalStorageKeys.AnswerCount, 5);
+  $: answerCountStore.set(answerCount);
+  $: answerCount = $answerCountStore;
 
-  let sendingIn: number = sendingInStart;
-  let sendingIntervalId: number;
+  $: showQuestionsStore = localStorage<boolean>(
+    LocalStorageKeys.SavedExport,
+    false
+  );
+  $: showQuestionsStore.set(showQuestions);
+  $: showQuestions = $showQuestionsStore;
 
-  const sendingInterval = () => (sendingIn -= 1000);
+  $: collapsePreviousStore = localStorage<boolean>(
+    LocalStorageKeys.CollapsePrevious,
+    false
+  );
+  $: collapsePreviousStore.set(collapsePrevious);
+  $: collapsePrevious = $collapsePreviousStore;
 
-  const resetSendingInterval = () => {
-    clearInterval(sendingIntervalId);
-    sendingIn = sendingInStart;
-  };
+  onMount(() => (ready = true));
 
-  $: {
-    resetSendingInterval();
-
-    if (questions !== "") {
-      sendingIntervalId = setInterval(sendingInterval, 1000);
-      debounceQuestions(questions);
-    }
-  }
+  $: questions !== "" && debounceQuestions(questions);
 
   const debounceQuestions = debounce(
     (oldQuestions: string) =>
       questions === oldQuestions && loadData(oldQuestions),
-    sendingInStart
+    500
   );
 
   const loadData = async (newQuestions: string) => {
     questions = "";
     loading = true;
 
-    for (const question of newQuestions
-      .split(/\r?\n/)
+    const questionsToSend = newQuestions
+      .split(/\r?[\n\?]/)
       .filter((line) => line !== "")
-      .reverse()) {
-      const response = await wretch($session.qnaMaker.endpoint)
-        .auth($session.qnaMaker.auth)
+      .reverse();
+
+    if (collapsePrevious) {
+      collapsedAnswerGroupIds = answerGroups.map(
+        (answerGroup) => answerGroup.id
+      );
+    }
+
+    for (const question of questionsToSend) {
+      const response = await wretch("qnaMaker/answer")
         .post({
-          question,
+          question: `${question}?`,
           rankerType: "QuestionOnly",
-          top: 3,
+          top: answerCount,
         })
         .json<IQnASearchResultList>();
 
@@ -84,7 +105,7 @@
 
       answerGroups = [
         {
-          question,
+          question: `${question}?`,
           answers: response.answers.map((answer) => ({
             answer: answer.answer,
             questions: answer.questions,
@@ -100,15 +121,15 @@
     loading = false;
   };
 
-  let copied: HTMLDivElement;
-  let copiedTimeoutId: number;
-
-  const copy = (question: string, answer: IAnswer) => (
+  const copy = (question: string) => (answer: IAnswer) => async (
     event: MouseEvent & {
       currentTarget: HTMLDivElement;
     }
   ) => {
-    copied.classList.remove("animate");
+    copied = {
+      top: `${event.pageY}px`,
+      left: `${event.pageX}px`,
+    };
 
     const temporaryElement = document.createElement("p");
 
@@ -121,129 +142,280 @@
     document.execCommand("copy");
     event.currentTarget.parentElement.removeChild(temporaryElement);
 
-    if (copiedTimeoutId !== undefined) {
-      clearTimeout(copiedTimeoutId);
-    }
-
-    copied.classList.add("animate");
-    copied.style.setProperty("--x", `${event.clientX}px`);
-    copied.style.setProperty("--y", `${event.clientY}px`);
-
-    copiedTimeoutId = setTimeout(() => {
-      copied.classList.remove("animate");
-      copiedTimeoutId = undefined;
-    }, 1500);
-
-    trainingPairs.set(answer.id, { question, answer });
+    questionAnswers.set(answer.id, { question, answer });
     trainThrottle();
-  };
-
-  const opacity = (node: HTMLElement, score: number) => {
-    node.style.setProperty("--opacity", `${score}`);
   };
 
   const trainThrottle = throttle(
     async () => {
-      await wretch($session.qnaMaker.trainEndpoint)
-        .auth($session.qnaMaker.auth)
-        .post({
-          feedbackRecords: [...trainingPairs.values()].map((trainingPair) => ({
-            userQuestion: trainingPair.question,
-            qnaId: trainingPair.answer.id,
-          })),
-        });
+      await wretch("qnaMaker/train").post({
+        feedbackRecords: [...questionAnswers.values()].map((trainingPair) => ({
+          userQuestion: trainingPair.question,
+          qnaId: trainingPair.answer.id,
+        })),
+      });
+
+      questionAnswers = new Map<number, IQuestionAnswer>();
     },
-    10000,
+    5000,
     { leading: false }
   );
 
-  const t = translate($session.translations);
+  const t = translate({
+    en_us: {
+      translation: {
+        questionsInTheBox: "Put questions here...",
+        numberOfAnswers: "Number of answers to fetch:",
+        showQuestions: "Show original questions?",
+        collapsePrevious: "Collapse previous questions?",
+        copied: "copied to clipboard",
+      },
+    },
+  });
 </script>
 
 <svelte:head>
-  <title>{$session.title}</title>
+  <title>Kwik</title>
 </svelte:head>
 
 {#if ready}
-  <h1 in:fly={{ x: 100 }}>{$session.title}</h1>
+  <h1>
+    <span in:fly={{ x: 100, duration: 500 }}>K</span><span
+      in:fly={{ x: 200, duration: 600 }}>w</span
+    ><span in:fly={{ x: 300, duration: 800 }}>i</span><span
+      in:fly={{ x: 400, duration: 1100 }}>k</span>
+  </h1>
 {:else}
   <h1>&nbsp;</h1>
 {/if}
 <section>
   <div class="questions">
     <div class="sticky">
-      <p>{$t("questionsInTheBox")}</p>
       {#if loading}
         <div class="loading"><Loading /></div>
       {:else}
-        <textarea bind:value={questions} />
-      {/if}
-      <div>
-        <button
-          disabled={questions === ""}
-          on:click={() => loadData(questions)}>
-          {#if questions === "" || sendingIn > 3000}
-            {$t("ask")}
-          {:else}
-            {`${$t("askingIn")} ${sendingIn / 1000} ${$t("askingInSeconds")}`}
+        <div class="overlayContext">
+          <textarea bind:value={questions} />
+          {#if questions === ""}
+            <div class="overlay">{$t`questionsInTheBox`}</div>
           {/if}
-        </button>
-      </div>
+        </div>
+      {/if}
+      <br />
+      <label class="group column overlayContext">
+        <input
+          class="slider"
+          type="range"
+          bind:value={answerCount}
+          min="1"
+          max="10" />
+        <div class="overlay">
+          {`${$t`numberOfAnswers`} ${answerCount}`}
+        </div>
+      </label>
+      <label class="group column overlayContext checkboxContext">
+        <input type="checkbox" bind:checked={showQuestions} />
+        <div class="checkbox" />
+        <div class="overlay">
+          {$t`showQuestions`}
+        </div>
+      </label>
+      <label class="group column overlayContext checkboxContext">
+        <input type="checkbox" bind:checked={collapsePrevious} />
+        <div class="checkbox" />
+        <div class="overlay">
+          {$t`collapsePrevious`}
+        </div>
+      </label>
+      <div />
     </div>
   </div>
   <div class="answers">
     {#if answerGroups.length > 0}
-      <p>{$t("clickToCopy")}</p>
       {#each answerGroups as answerGroup (answerGroup.id)}
-        <div class="answerGroup" transition:fly={{ x: 50 }}>
-          <div class="question">{answerGroup.question}</div>
-          {#each answerGroup.answers as answer}
-            <div>
-              <div
-                class="answer"
-                title={answer.questions.join("\n")}
-                use:opacity={answer.score}
-                on:click={copy(answerGroup.question, answer)}>
-                {answer.answer}
-              </div>
-            </div>
-          {/each}
+        <div animate:flip={{ duration: 300 }}>
+          <Answer
+            {answerGroup}
+            {showQuestions}
+            click={copy(answerGroup.question)}
+            open={!collapsedAnswerGroupIds.includes(answerGroup.id)} />
         </div>
       {/each}
     {/if}
   </div>
 </section>
-<div class="copied" bind:this={copied}>
-  {$t("copied")}
-</div>
+{#if copied}
+  {#key copied}
+    <div
+      class="copied"
+      use:property={["top", copied.top]}
+      use:property={["left", copied.left]}>
+      {$t("copied")}
+    </div>
+  {/key}
+{/if}
 
 <style>
+  :global(main) {
+    background: linear-gradient(135deg, hsl(236, 9%, 98%), hsl(201, 33%, 90%))
+      fixed;
+    position: relative;
+  }
+
+  .group {
+    display: flex;
+    margin-bottom: 0.5em;
+  }
+
+  .column {
+    flex-direction: column;
+  }
+
+  .item {
+    flex: 1;
+  }
+
   h1 {
     font-size: 5em;
     text-transform: uppercase;
     font-weight: 700;
     margin: 0 0 0.5em 0.5em;
+    color: #00a2ff;
+  }
+
+  h1 span {
+    display: inline-block;
   }
 
   section {
     flex-direction: row;
+    margin: 0 0 0 3em;
+  }
+
+  .questions {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    min-width: 30em;
+  }
+
+  .questions .sticky {
+    position: sticky;
+    top: 1em;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .questions .loading {
+    min-height: 20em;
+    padding: 0.5em;
+  }
+
+  .questions textarea {
+    resize: none;
+    min-height: 20em;
+    height: 100%;
+    width: 100%;
+    font-size: 1em;
+    font-family: inherit;
+    border-radius: 1em;
+    border-width: 0.2em;
+    border-color: transparent;
+    padding: 0.5em;
+  }
+
+  .questions .overlayContext {
+    position: relative;
+  }
+
+  .questions .overlay {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    place-content: center;
+    pointer-events: none;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .questions textarea + .overlay {
+    text-align: center;
+    font-size: 2em;
+    font-weight: 800;
+    color: rgb(0 0 0 / 15%);
+  }
+
+  .questions textarea:focus {
+    border-color: #00a2ff;
+    outline: none;
+  }
+
+  .checkboxContext {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  input[type="checkbox"] {
+    display: none;
+  }
+
+  input[type="checkbox"] + .checkbox {
+    height: 2em;
+    width: 2em;
+    background: hsl(201, 33%, 90%);
+    border-radius: 0.2em;
+  }
+
+  input[type="checkbox"]:checked + .checkbox {
+    background: #00a2ff;
+  }
+
+  input[type="range"] {
+    appearance: none;
+    background: hsl(201, 33%, 90%);
+    border-radius: 0.2em;
+    width: 100%;
+    margin: 0;
+    outline: none;
+  }
+
+  .questions input[type="range"] + .overlay {
+    font-weight: 600;
+    padding: 0 0 0 0.5em;
+    color: #00a2ff;
+  }
+
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    background: #00a2ff;
+    height: 2em;
+    width: 0.5em;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  input[type="range"]::-moz-range-thumb {
+    -webkit-appearance: none;
+    background: #00a2ff;
+    height: 2em;
+    width: 0.5em;
+    border-radius: 5px;
+    cursor: pointer;
   }
 
   .copied {
-    position: fixed;
-    pointer-events: none;
-    display: none;
+    position: absolute;
     transform: translate(-50%, -50%);
+    left: var(--left);
+    top: var(--top);
+    pointer-events: none;
     font-weight: 600;
     color: #00a2ff;
     font-variant: small-caps;
-    left: var(--x);
-    top: var(--y);
-  }
-
-  :global(.copied.animate) {
-    display: block !important;
     animation: zoom 1.2s ease-out forwards;
+    white-space: nowrap;
   }
 
   @keyframes zoom {
@@ -258,98 +430,10 @@
     }
   }
 
-  .questions {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-  }
-
-  .questions .sticky {
-    position: sticky;
-    top: 0;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .questions .loading {
-    min-height: 20em;
-    padding: 0.5em;
-  }
-
-  .questions textarea {
-    resize: none;
-    min-height: 20em;
-    font-size: 1em;
-    font-family: inherit;
-    border-radius: 1em;
-    border-width: 0.2em;
-    padding: 0.5em;
-  }
-
-  .questions textarea:focus {
-    border-color: #00a2ff;
-    outline: none;
-  }
-
-  .questions button {
-    margin-top: 1em;
-    background: #00a2ff;
-    border: none;
-    color: white;
-    padding: 0.5em 1em;
-    font-weight: 600;
-    font-size: 1.25em;
-    border-radius: 0.5em;
-  }
-
-  .questions button:disabled {
-    background: #577383;
-    color: rgb(194, 194, 194);
-  }
-
-  .questions button:hover {
-    background: #0cb2ff;
-    cursor: help;
-  }
-
-  .questions button:disabled:hover {
-    background: #577383;
-    cursor: not-allowed;
-  }
-
-  .questions button:focus:not(:disabled) {
-    outline: none;
-  }
-
   .answers {
     flex: 2;
-    padding: 0 0 0 1em;
-  }
-
-  .answers .answerGroup {
-    margin: 0.5em 0;
-  }
-
-  .answers .question {
-    font-weight: 500;
+    padding: 0 0 5em 1em;
     color: #848484;
-    font-size: 0.8em;
-  }
-
-  .answers .answer {
-    margin: 0.1em 0 0.1em 1em;
-    background: #e8e8e8;
-    border-radius: 0.5em;
-    padding: 0.5em;
-    display: inline-block;
-    transition: 0.3s ease;
-    opacity: var(--opacity);
-  }
-
-  .answers .answer:hover {
-    background: #6cd6ff;
-    opacity: 1;
-    cursor: copy;
   }
 
   @media (max-width: 800px) {
